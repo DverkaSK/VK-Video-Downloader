818513b763498c112cee510eefb00acfc879bba1
Kirill Volozhanin <volozhaninkirill@gmail.com>
2025-12-20 13:49:28 +0500

    refactor: partial Chrome desktop script refactoring

--- chromium/scripts/desktop.js
+++ chromium/scripts/desktop.js
@@ -10,57 +10,64 @@ new MutationObserver(() => {
     checkerHasBeenCalled = false;
     showPanelHasBeenCalled = false;
 
-    const old_panel = document.querySelector("#vkVideoDownloaderPanel");
-    if (old_panel !== null) {
-      old_panel.remove();
-    }
+    document.querySelector("#vkVideoDownloaderPanel")?.remove();
   }
 
-  if (
-    (/z=(?:video|clip)/.test(location.search) ||
-      /^\/(?:video|clip)[^\/s]+$/.test(location.pathname) ||
-      /^\/playlist\/[\d-]+/.test(location.pathname)) &&
-    !checkerHasBeenCalled
-  ) {
+  const isVideoUrl = /z=(?:video|clip)/.test(location.search)
+    || /^\/(?:video|clip)[^\/s]+$/.test(location.pathname)
+    || /^\/playlist\/[\d-]+/.test(location.pathname);
+
+  if (isVideoUrl && !checkerHasBeenCalled) {
     checkerHasBeenCalled = true;
     const checker = setInterval(() => {
+      if (showPanelHasBeenCalled) {
+        clearInterval(checker);
+        return;
+      }
+
       const player = document.querySelector("#video_player video")
         || document.querySelector("#video_player .shadow-root-container")?.shadowRoot?.querySelector("video");
-      if (!showPanelHasBeenCalled && player) {
-        showPanelHasBeenCalled = true;
-        clearInterval(checker);
+
+      const iframe = player ? null : document.querySelector("#video_player iframe");
+
+      if (!player && !iframe) {
+        return;
+      } else if (player) {
         showDownloadPanel();
-      } else if (!showPanelHasBeenCalled && document.querySelector("#video_player iframe")) {
-        showPanelHasBeenCalled = true;
-        clearInterval(checker);
+      } else if (iframe) {
         showErrorPanel();
       }
+
+      showPanelHasBeenCalled = true;
     }, 500);
   }
 }).observe(document.body, { subtree: true, childList: true });
 
 function showDownloadPanel() {
   const script = document.createElement("script");
-  script.charset = "utf-8";
-  script.type = "text/javascript";
+
   script.src = chrome.runtime.getURL("scripts/desktop-injection.js");
+  script.onload = () => script.remove();
+
   document.body.appendChild(script);
 }
 
 function showErrorPanel() {
   const label = document.createElement("span");
-  label.innerText = "Видео со стороннего сайта. Воспользуйтесь инструментами для скачивания с него.";
+  label.textContent = "Видео со стороннего сайта. Воспользуйтесь инструментами для скачивания с него.";
 
   const panel = document.createElement("div");
   panel.id = "vkVideoDownloaderPanel";
-  panel.style.position = "fixed";
-  panel.style.left = "16px";
-  panel.style.bottom = "16px";
-  panel.style.zIndex = "2147483647";
-  panel.style.padding = "4px";
-  panel.style.color = "#fff";
-  panel.style.backgroundColor = "#07f";
-  panel.style.border = "1px solid #fff";
+  Object.assign(panel.style, {
+    position: "fixed",
+    left: "16px",
+    bottom: "16px",
+    zIndex: "2147483647",
+    padding: "4px",
+    color: "#fff",
+    backgroundColor: "#07f",
+    border: "1px solid #fff",
+  });
   panel.appendChild(label);
 
   document.body.appendChild(panel);
